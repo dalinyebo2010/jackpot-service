9b5a2d58f4bf95d0579b830495c6167ef96ef287
Marks Bhele <mthawelanga77@yahoo.com>
2026-06-12 07:39:09 +0200

    Initial commit

---
+++ src/test/java/com/jackpot/JackpotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackpotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
